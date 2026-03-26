e57e071f5c1f9a0eb462b828b1de6ba700380c6c
Vany Eka <vanyekak@gmail.com>
2026-03-26 08:32:50 +0700

    Menambahkan Import HttpModule

--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { LocationsController } from './locations.controller';
 
 @Module({
+  imports: [HttpModule, ConfigModule],
   controllers: [LocationsController]
 })
 export class LocationsModule {}
